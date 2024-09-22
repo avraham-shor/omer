71c2b40aef24bbdaf72de3ec3afcb4ed2bfcfc37
Avraham Shor <57142560+avraham-shor@users.noreply.github.com>
2024-09-22 09:07:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -374,27 +374,27 @@ function setMessages(day, specifyMsg) {
         sizeForAndroid = 0.8;
 
         // Apply styles to elements
-        document.querySelector('.box-shma').style.fontSize = '2.6rem';
-        document.querySelector('.shkiah').style.fontSize = '2.6rem';
-        document.querySelector('.day').style.fontSize = '3rem';
+        document.querySelector('.box-shma').style.fontSize = '2.9rem';
+        document.querySelector('.shkiah').style.fontSize = '2.9rem';
+        document.querySelector('.day').style.fontSize = '3.5rem';
         
         let timeElement = document.querySelector('.time');
-        timeElement.style.fontSize = '3rem';
-        timeElement.style.top = '10vh';
+        timeElement.style.fontSize = '4.7rem';
+        timeElement.style.top = '9vh';
     
         let titleElement = document.querySelector('.title');
-        titleElement.style.bottom = '8vh';
+        titleElement.style.bottom = '6vh';
         titleElement.style.fontSize = '2.6rem';
     
         let middleBoxElement = document.querySelector('.middle-box');
-        middleBoxElement.style.fontSize = '2rem';
-        middleBoxElement.style.top = '43vh';
+        middleBoxElement.style.fontSize = '2.9rem';
+        middleBoxElement.style.top = '46vh';
         middleBoxElement.style.right = '10vw';
     
-        document.querySelector('.msg').style.top = '27vh';
-        document.querySelector('.daf').style.fontSize = '2rem';
-        document.querySelector('.date').style.fontSize = '2rem';
-        document.querySelector('.daf-title').style.fontSize = '2rem';
+        document.querySelector('.msg').style.top = '33vh';
+        document.querySelector('.daf').style.fontSize = '2.9rem';
+        document.querySelector('.date').style.fontSize = '2.9rem';
+        document.querySelector('.daf-title').style.fontSize = '2.9rem';
     }
 
     msgObj.style.fontSize = (10 - msgText.length / 12) * sizeForAndroid + 'rem';
