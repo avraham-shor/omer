3d6e009c0483846a2fd1c02d30c077676e2e36bf
Avraham Shor <57142560+avraham-shor@users.noreply.github.com>
2024-12-31 17:50:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -625,7 +625,7 @@ function setShtibelSetings() {
         titleShkiahElement.style.bottom = '8.6vh';
         titleShkiahElement.style.fontSize = '3rem';
     
-        document.querySelector('.msg').style.top = '38vh';
+        document.querySelector('.msg').style.top = '35vh';
         document.querySelector('.daf').style.fontSize = '2.4rem';
         document.querySelector('.date').style.fontSize = '2.4rem';
         document.querySelector('.daf-title').style.fontSize = '1.8rem';
