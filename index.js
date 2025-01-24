9192ed9fd512be6f84f4587e3eda8cd67892ef90
avraham-shor-venn <avraham.shor@venn.city>
2025-01-24 15:45:09 +0200

    add redirect for Hagrala

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 //window.location.replace("https://avraham-shor-venn.github.io/avot/");
 
 const toDayNow = new Date();
-if (toDayNow.getHours() == 18 && toDayNow.getDate() == 18 && toDayNow.getMonth() == 0) {
+if (toDayNow.getHours() == 18 && toDayNow.getMinutes() > 30 && toDayNow.getDate() == 25 && toDayNow.getMonth() == 0) {
     window.location.replace("https://avraham-shor-venn.github.io/avot/")
 } {
     
