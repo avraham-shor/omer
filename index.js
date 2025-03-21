2d186ee96af0df07156c01f6fa72705d356e5423
avraham-shor-venn <avraham.shor@venn.city>
2025-03-21 07:46:53 +0200

    remake tehilim

--- index.js
+++ index.js
@@ -178,7 +178,7 @@ function refresh() {
         specifyMsg.push({color: 'red', text: 'מוריד הטל'});
     }
 
-    if (false && t(isShowTehilim) && tehilimByDays[dayInMonth] && tehilimByDays[dayInMonth].length ) {
+    if (t(isShowTehilim) && tehilimByDays[dayInMonth] && tehilimByDays[dayInMonth].length ) {
         specifyMsg.push({color: 'darkblue', text: `%פרקי תהלים@%` + tehilimByDays[dayInMonth] || ''});
     }
 
